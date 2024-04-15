62deb4e92ed11230c5f06e731e3ebdf9b454a069
Kush Shah <kush@unc.edu>
2024-04-15 19:00:44 -0400

    Complete about section

--- kushs/src/components/Info.tsx
+++ kushs/src/components/Info.tsx
@@ -18,10 +18,19 @@ const Info: React.FC = () => {
                     Kush Shah
                 </h1>
                 <p>
-                    Howdy! I am a Senior at the University of North Carolina
-                    at Chapel Hill studying Computer Science and Business.
+                    Hey there, thanks for checking out my website. I built this from scratch using React.js, TypeScript, and SCSS.
+                    <br/>
+                    I am a student at UNC Chapel Hill studying Computer Science and Business Administration, with a minor in Philosophy, Politics, and Economics.
+                    <br/>
+                    In my professional life, I'm passionate about building things for people to use, and talking to them about how they use them.
+                    I strive to understand the people I'm building for, and to make their lives
+                    better through the products I create.
+                    <br/>
+                    When I'm left with time to myself, I fill it taking photos of the people and the world around me, travelling the world, listening to music, and
+                    playing with different technologies. I enjoy going outside and exploring the world around me, striving to do something each day that pushes me outside my
+                    comfort zone. I'd love to chat with you about any of these things, so feel free to reach out!
+
                 </p>
-                <p>This page is still under construction :)</p>
                 <a href="https://www.linkedin.com/in/kushsha/" target="_blank" rel="noopener noreferrer"
                    className='icon-link'>
                     <FaLinkedinIn size={64}/>
